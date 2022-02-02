cafc54c6fb5d1f84f1ab3fd08d96fe71b2a62bcb
ErickSchuenck <esfmokdeci@gmail.com>
2022-02-02 11:13:51 -0300

    primeira versão

---
+++ main.js
@@ -0,0 +1,8 @@
+function flip(e){
+    if(document.querySelector('.card_front').style.display == 'none'){
+        document.querySelector('.card_front').style.display = '';
+    } else {
+        document.querySelector('.card_front').style.display = 'none';
+    }
+} 
+
