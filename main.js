2ce0809373f131b4819a4211a5a2af949c077ba7
ErickSchuenck <esfmokdeci@gmail.com>
2022-02-02 15:00:53 -0300

    primeiro update

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+let card_number = parseInt(prompt("Com quantos pares você quer jogar? (De 1 a 7)"));
+while (card_number>7){
+    card_number = parseInt(prompt("Com quantos pares você quer jogar? (De 1 a 7)"))
+}
+
 function flip(e){
     if(document.querySelector('.card_front').style.display == 'none'){
         document.querySelector('.card_front').style.display = '';
